a204ce0a0d2fc24974c28df924bac08dd4ab76c0
Gwen <itsgwenhey@gmail.com>
2020-08-25 17:42:42 -0500

    make render function work
    make the render function iterate through the books array and render each one with it's information in html. Start adding the add book function

--- library.js
+++ library.js
@@ -1,4 +1,6 @@
-let library = [];
+let library = document.getElementById("library");
+
+let books = [];
 
 class Book {
     constructor(title,author,pages,read) {
@@ -8,22 +10,57 @@ class Book {
         this.read = read;
         this.isRead = this.read ? 'read it.' : 'not read it.';
     }
-    info() {
-        return this.title + ' by '+this.author+' is '+this.pages+' pages long, and i have '+this.isRead;
-    };
 }
 
 const addBook = (title,author,pages,read) => {
     let newBook = new Book(title,author,pages,read);
-    library.push(newBook);
+    books.push(newBook);
 }
 
 const render = () => {
-    
+    library.innerHtml = '';
+    for (book in books){
+        //create div
+        let containerDiv = document.createElement('div');
+        //append div to library
+        library.appendChild(containerDiv);
+        //give div a bootstrap class card
+        containerDiv.classList.add('card');
+        containerDiv.classList.add('bookCard');
+
+        //create title
+        let bookTitle = document.createElement('h3');
+        //append title to div
+        containerDiv.appendChild(bookTitle);
+        //give title class card-title
+        bookTitle.classList.add('card-title');
+        //give title text
+        bookTitle.textContent = books[book].title;
+
+        //create page count
+        let pageCount = document.createElement('p');
+        //append page count to div
+        containerDiv.appendChild(pageCount);
+        //give page count class
+        pageCount.classList.add('pageCount');
+        //give page count text
+        pageCount.textContent = books[book].pages + ' pages.';
+        
+        //create read status
+        let read = document.createElement('p');
+        //append read to div
+        containerDiv.appendChild(read);
+        //give read status class
+        read.classList.add('readStatus');
+        //give read status text
+        read.textContent = books[book].read ? 'You\'ve read this book.' : 'You haven\'t read this book';
+
+    }
 }
 
-let thisbook = addBook('book1','me',44,true);
-let thatbook = addBook('book2','you',65,false);
-let anotherbook = addBook('book3','someone',99,false);
-let fourhtbook= addBook('book4','a person',4,true);
+let thisbook = addBook('Book1','me',44,true);
+let thatbook = addBook('Book2','you',65,false);
+let anotherbook = addBook('Book3','someone',99,false);
+let fourhtbook= addBook('Book4','a person',4,true);
 
+document.addEventListener("DOMContentLoaded", render);
